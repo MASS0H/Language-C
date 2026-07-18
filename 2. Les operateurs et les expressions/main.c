6cacc135d54a9f0c96ff0927aa8df63c8bed3a9f
Hossam Ouammi <ouammi.hossam@gmail.com>
2026-07-18 17:33:52 +0100

    comit 2

--- 2. Les operateurs et les expressions/main.c
+++ 2. Les operateurs et les expressions/main.c
@@ -7,18 +7,57 @@ une operateur est un symbole qui represente une action et qui prend des valeurs 
 
 Exemple : dans "a + b", + est un operateur qui prend 2 valeurs ou plus en entree et qui retourne la somme de ces valeurs
 
+=> Les operateurs classiques
+1- Addition (+)
+2- Soustraction (-)
+3- Multiplication (*) , n'utilse jamais le (x);
+4- Division (/);
+
+=> Operateurs de programmation
+1 - affectation (=)
+2- incrementation (++)
+3- decrementation (--)
+
+
+
 
 
 */
 
 
 int main() {
-    int a, b;
+    int a, b, c;
     printf("veuillez saisir la valeur de a : ");
     scanf("%d", &a);
     printf("veuillez saisir la valeur de b : ");
     scanf("%d", &b);
 
-    printf(" la somme de a est b :  %d \n" ,  a + b );
-    return 0;
+    // utilisation de l'operateur d'affectation : signifie "affecter la valeur de 'b' à 'a'
+    // c'est la valeur a gauche qui change de valeur , et la valeur a droite ne se change pas
+    a = b ;
+    printf("%d \n", a);
+    printf("%d \n", b);
+    printf("%d \n", c);
+    printf("==================");
+    c = a + b;
+    printf("%d \n", a);
+    printf("%d \n", b);
+    printf("%d \n", c);
+    printf("==================");
+
+    b = a;
+    printf("%d \n", a);
+    printf("%d \n", b);
+    printf("%d \n", c);
+    printf("==================");
+
+    c = a * b;
+    printf("%d \n", a);
+    printf("%d \n", b);
+    printf("%d \n", c);
+    printf("==================");
+
+
+
+  
 }
